201d5333982700e399a904469d17ff35eebf1f9f
kalsowerus <roy.geesing@hispeed.ch>
2017-11-02 10:58:50 +0100

    fixed paths

--- convert.php
+++ convert.php
@@ -2,7 +2,7 @@
 $target_file = "uploads/" . pathinfo($_FILES["file"]["name"], PATHINFO_FILENAME) . ".mp4";
 $source_file = $_FILES["file"]["tmp_name"];
 
-echo "<pre>" . shell_exec("C:\\Users\\geroy\\Downloads\\ffmpeg-3.4-win64-static\\ffmpeg-3.4-win64-static\\bin\\ffmpeg.exe -i $source_file -vcodec h264 -acodec aac $target_file 2>&1") . "</pre>";
+echo "<pre>" . shell_exec("lib\\ffmpeg.exe -i $source_file -vcodec h264 -acodec aac $target_file 2>&1") . "</pre>";
 //echo move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
 
 echo "<br>";
@@ -13,7 +13,7 @@ echo "<br>";
 echo $_FILES["file"]["tmp_name"];
 
 echo "<br>";
-echo "<a href=\"/poc/$target_file\">Video</a>";
+echo "<a href=\"/M152/$target_file\">Video</a>";
 
 //shell_exec("C:\Users\geroy\Downloads\ffmpeg-3.4-win64-static\ffmpeg-3.4-win64-static\bin\ffmpeg.exe ");
 ?>
